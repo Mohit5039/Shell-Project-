65bb6c2742a9a78f22b9db99d0d34880f0ec26b5
Mohit5039 <mohitshekhawat2323@gmail.com>
2025-02-05 20:23:36 +0530

    type built in

--- app/main.js
+++ app/main.js
@@ -16,6 +16,18 @@ function prompt() {
     else if (answer.startsWith('echo')){
       console.log(answer.slice(5)) ;  
     }
+    else if (answer.startsWith("type ")) {
+      let command = answer.slice(5).trim();  // Remove "type " and trim spaces
+      
+      if (command === "") {
+        console.log("Usage: type [command]"); // Handling empty case
+      } else if (command === "exit" || command === "echo") {
+        console.log(`${command} is a shell builtin`);
+      } else {
+        console.log(`${command}: not found`);
+      }
+    }
+    
     else{
     console.log(`${answer}: command not found`);
     }
@@ -23,3 +35,5 @@ function prompt() {
   });
 }
 prompt(); // Start the prompt loop
+
+
