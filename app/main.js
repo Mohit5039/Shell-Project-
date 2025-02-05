5a0b7c419e263b6e281a07417dbd48a0f4520777
Mohit5039 <mohitshekhawat2323@gmail.com>
2025-02-05 17:52:08 +0530

    echo

--- app/main.js
+++ app/main.js
@@ -13,7 +13,7 @@ function prompt() {
       process.exit(0);
     }
     else if (answer.startsWith('echo')){
-      console.log(answer.slice(5)) ; // echo command 
+      console.log(answer.slice(5)) ;  
     }
     else{
     console.log(`${answer}: command not found`);
