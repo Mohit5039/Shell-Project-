57b952d1ef725a070ae8485a931f9fa92c85d37b
Mohit5039 <mohitshekhawat2323@gmail.com>
2025-02-24 14:42:57 +0530

    asdfghj

--- app/main.js
+++ app/main.js
@@ -80,7 +80,7 @@ function prompt() {
       return;
     } 
     else if (command === "echo") {
-      console.log(commandargs.map(arg => arg.replace(/\\n/g, "n")).join(" "));
+      console.log(commandargs.map(arg => arg.replace(/\\n/g, "\\n")).join(" "));
     }
     else if (command === "pwd") {
       console.log(process.cwd()); 
